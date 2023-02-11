91eca5758b83009840d41bed16b04463a4ddb4e4
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-11 11:48:26 +0100

    javascript on image slider

--- javascriptTimer/index.js
+++ javascriptTimer/index.js
@@ -12,11 +12,11 @@ setTimeout(showMessage, 3000);*/
 let colourChanger = document.getElementById("colour-changer");
 
 let colours =["red","blue","green","pink","yellow"];
-
+ let image = ["image1.jpg", "image2.jpg", "image3.jpg","image4.jpg","image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpg"]
 let counter = 0;
 
 
-function changeColor(){
+/*function changeColor(){
 
         if(counter >=  colours.length){
             counter = 0;
@@ -28,9 +28,24 @@ function changeColor(){
 
 
 
-let myTimer = setInterval(changeColor, 3000)
+let myTimer = setInterval(changeColor, 3000)*/
+function changeImage(){
 
-colourChanger.onclick = function(){
+    if(counter >=  image.length){
+        counter = 0;
+    }
+    colourChanger.style.backgroundImage = "url(images/"+ image[counter] +")";
+
+    counter++;
+}
+
+
+
+
+let myTimer = setInterval(changeImage, 3000)
+
+
+colourChanger.ondblclick = function(){
 
     clearInterval(myTimer);
     colourChanger.innerHTML = "Timer stopped"
