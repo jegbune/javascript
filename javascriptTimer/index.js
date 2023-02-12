0ded53c6702a7deffb0e764e9702deea663a4531
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-12 16:13:14 +0100

    javascript lesson for color change container

--- javascriptTimer/index.js
+++ javascriptTimer/index.js
@@ -12,11 +12,11 @@ setTimeout(showMessage, 3000);*/
 let colourChanger = document.getElementById("colour-changer");
 
 let colours =["red","blue","green","pink","yellow"];
- let image = ["image1.jpg", "image2.jpg", "image3.jpg","image4.jpg","image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpg"]
+let image = ["image1.jpg", "image2.jpg", "image3.jpg","image4.jpg","image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpg"]
 let counter = 0;
 
 
-/*function changeColor(){
+function changeColor(){
 
         if(counter >=  colours.length){
             counter = 0;
@@ -28,8 +28,8 @@ let counter = 0;
 
 
 
-let myTimer = setInterval(changeColor, 3000)*/
-function changeImage(){
+let myTimer = setInterval(changeColor, 3000)
+/*function changeImage(){
 
     if(counter >=  image.length){
         counter = 0;
@@ -49,4 +49,4 @@ colourChanger.ondblclick = function(){
 
     clearInterval(myTimer);
     colourChanger.innerHTML = "Timer stopped"
-}
+}*/
