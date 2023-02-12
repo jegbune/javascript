a325528b3379ac47f0705e69f2e59633e4c8550b
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-12 22:35:22 +0100

    javascript product slide picture

--- javascriptTimer/index2.js
+++ javascriptTimer/index2.js
@@ -1,17 +1,22 @@
-let s = document.getElementsByClassName("product-picture");
-let pics = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"];
-let count = 0;
-function slide(){
+let picture = document.getElementsByClassName("picture");
+let pic = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg"];
+let counts = 0;
+
+
+function slide1(){
     
-    if(count >= pics.length){
-        count = 0;
+    for(let i = 0; i < picture.length; i++){
+        
+        picture[i].style.backgroundImage = "url(images/" + pic[count]+ ")";
+        counts++;
+
     }
-    let i;
-    for(i = 0; i < pictur.length; i++)
-    slides[i].
-    // slides[i].style.backgroundImage = "url(images/" + pics[count] + ")";
-    count++;
+
+    if(counts >= pic.length){
+        counts = 0;
+    }
+    
 
 }
 
-setInterval(slide, 3000);
+setInterval(slide1, 5000);
