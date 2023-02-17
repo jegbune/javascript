a28d6387adf64a88d3be7e3aaaa2c88ee743f7a3
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-17 18:42:52 +0100

    javascript project on roman Numeral to Number

--- RomanNumeral/script.js
+++ RomanNumeral/script.js
@@ -1,8 +1,10 @@
 let input = document.querySelector("#numbers");
+let input1 = document.querySelector("#numbers1");
 let button = document.querySelector("#Btn");
+let button1 = document.querySelector("#Btn1");
 let result = document.querySelector(".result");
 
-// Constan variable
+// Constant variables
 const lookupTable = {
     M: 1000,
     CM: 900,
@@ -19,24 +21,80 @@ const lookupTable = {
     I: 1,
 
 }
-let accumulator = '';
-button.addEventListener('click', function(){
-   
-let inputValue = input.value;
-// alert(input.value);
+//when you click on the button it will make the conversion
+// button.addEventListener('click', function(){
+//     let accumulator = '';
+//     let inputValue = input.value;
+//     //Maximum number that can be calculated is 3999.
+//     if(inputValue > 3999){
+//         inputValue= ''
+//     }
+
+// // alert(input.value);
     
-    for(const i in lookupTable){
-        const numberValue = lookupTable[i];
+//     for(const i in lookupTable){
+//         const numberValue = lookupTable[i];
     
-        while(numberValue <= inputValue){
+//         while(numberValue <= inputValue){
+        
+//                 inputValue -= numberValue;
+//                 accumulator += i;
+    
+//         }
+//     }
+//     result.setAttribute('style', "display:flex");
+//     result.textContent = accumulator;
+    
+//             // alert(accumulator);
+
+//         });
+
+button1.addEventListener('click', function(){
+    let value = '';
+    let input1Value = input1.value;
+    //Maximum number that can be calculated is 3999.
+    // if(inputValue > 3999){
+    //     inputValue= ''
+    // }
+    let input2 = input1Value.toUpperCase();
+    // alert(input2)
+       let input3 = input2.split("")
+        // alert(typeof input3)
+    
+        for(const i in input3){
+            const roman = input3[i];
+            for(const key in lookupTable){
+             const numberValue1 = lookupTable[key];
+            alert(numberValue1);
+        if(roman === key){
+            alert();
+                
+            }
+            
+        }
+       }
+            // alert(input3[i])
+        // if(input2 === i){
+            // if(input2 >= i){
+                // alert(numberValue1);
+
+            // }
+
+        // }
+
+
+        /*while(numberValue <= inputValue){
         
                 inputValue -= numberValue;
                 accumulator += i;
     
         }
     }
-            result.textContent = accumulator;
-            // alert(accumulator);
+    result.setAttribute('style', "display:flex");
+    result.textContent = accumulator;
+    
+            // alert(accumulator);*/
+    // }
 
-return; 
-});
+        });
+
